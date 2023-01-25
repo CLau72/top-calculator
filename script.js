e1c85de8e06e93a8b0ede75150f7d12667e5fb8a
Carson Lau <carselau@gmail.com>
2023-01-24 23:24:04 -0500

    add number input functionality

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const display = document.querySelector('.display')
+
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener('click',() => {
+        if (display.textContent === '0') {
+            display.textContent = '';
+        }
+        display.textContent += numberButton.textContent;
+    });
+})
+
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click' ,() => {
+    display.textContent = 0;
+})
+
+function updateDisplay(value) {
+  return 0;    
+}
+
+// Basic Math Functions
 
 function add(a,b) {
     return a + b;
