5bbee6831f3ad38999323e485a9ffaacc910c59c
Carson Lau <carselau@gmail.com>
2023-01-31 14:38:14 -0500

    Add comments like a programmer boi

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Variables for math operations
 let firstDigit = 0;
 let secondDigit = 0;
 let operand = "";
 let result = 0;
 
+// Variables for DOM elements
 const display = document.querySelector('.display')
 const numberButtons = document.querySelectorAll('.number');
 const clearButton = document.querySelector('.clear');
 const operandButtons = document.querySelectorAll('.operator')
 const equalButton = document.querySelector('.equal')
 
+// Number buttons change display
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click',() => {
         if (display.textContent === '0') {
@@ -20,10 +23,12 @@ numberButtons.forEach((numberButton) => {
     });
 })
 
+// Clear button clears display and all math variables
 clearButton.addEventListener('click' ,() => {
     display.textContent = 0;
 })
 
+// Evaluate the math operation
 equalButton.addEventListener('click',() => {
     if (firstDigit && secondDigit && operand)
     result = operate(operand, firstDigit, secondDigit)
